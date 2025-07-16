b0b733b5e9f2173be55796e9fa334b763be37429
Razan Khammash <124376172+Razan-kh@users.noreply.github.com>
2025-07-16 21:01:23 +0300

    Update Inventory.cs
    Adding space
    
    Co-authored-by: nortega-fh <156388988+nortega-fh@users.noreply.github.com>

--- Inventory.cs
+++ Inventory.cs
@@ -7,6 +7,7 @@ namespace InventoryManagement
     class Inventory
     {
         List<Product> products = new List<Product>();
+
         public void AddProduct(string name, int quantity, int price)
         {
             Product p = new Product(name, price, quantity);
