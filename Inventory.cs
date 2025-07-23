020872f0310e7beb8de30eb80d75cfe219a9f065
unknown <you@example.com>
2025-07-24 00:27:27 +0300

    overriding the ToString method

--- Inventory.cs
+++ Inventory.cs
@@ -9,18 +9,16 @@ class Inventory
     List<Product> products = new List<Product>();
        
         public void SearchProduct()
-        {    
+        {
+        products.Add(new Product { Name = "F", Price = 5, Quantity = 7 });
             Console.Write("Enter the name of the product to search: ");
             string name = Console.ReadLine()?.Trim() ?? "";
 
             Product? product = products.FirstOrDefault(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
 
-            if (product != null)
+            if (product is not null)
             {
-                Console.WriteLine($"\nProduct Found:");
-                Console.WriteLine($"Name: {product.Name}");
-                Console.WriteLine($"Price: ${product.Price}");
-                Console.WriteLine($"Quantity: {product.Quantity}");
+                Console.WriteLine(product);
             }
             else
             {
--- Product.cs
+++ Product.cs
@@ -2,9 +2,18 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Dynamic;
 namespace InventoryManagement;
-    class Product
+
+class Product
+{
+    public required string Name { set; get; }
+    public required int Price { set; get; }
+    public required int Quantity { set; get; }
+
+    public override string ToString()
     {
-        public required string Name { set; get; }
-        public  required int Price { set; get; }
-        public  required int Quantity { set; get; }
+        return $"\nProduct Found:\n" +
+           $"Name: {this.Name}\n" +
+           $"Price: ${this.Price}\n" +
+           $"Quantity: {this.Quantity}";
+    }    
     }
